2cce78f3e9c358ea926ca32f24d38f3945ad7d94
Adam zahi <zahi.adem10@gmail.com>
2025-07-07 17:11:25 +0100

    changing params props

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -5,30 +5,27 @@ import { json } from '@codemirror/lang-json';
 import { useEffect, useState } from 'react';
 import { JsonData } from '@prisma/client';
 
-interface SharedJsonProps {
-    params: {
-        id: string;
-    };
-}
-export default function SharedJson({ params }: SharedJsonProps) {
+type PageProps = {
+    params: { id: string }
+    }
+
+export default function Page({ params }: PageProps) {
     const { id } = params;
     const [jsonData, setJsonData] = useState<JsonData>();
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const fetchData = async () => {
-        try {
-            const response = await fetch(`/api/json/${id}`);
-            const data = await response.json();
-
-            setJsonData(data);
-            setLoading(false);
-        } catch (error) {
-            console.error('Failed to fetch data:', error);
-            setLoading(false);
-        }
+            try {
+                const response = await fetch(`/api/json/${id}`);
+                const data = await response.json();
+                setJsonData(data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Failed to fetch data:', error);
+                setLoading(false);
+            }
         };
-
         fetchData();
     }, [id]);
 
@@ -37,14 +34,14 @@ export default function SharedJson({ params }: SharedJsonProps) {
     }
     return (
         <div className='mt-8 space-y-4'>
-        <h1 className='text-2xl underline font-bold'>{jsonData?.name}</h1>
-        <CodeMirror
-            value={jsonData?.content || ''}
-            height='400px'
-            extensions={[json()]}
-            editable={false}
-            className='border shadow-sm'
-        />
+            <h1 className='text-2xl underline font-bold'>{jsonData?.name}</h1>
+            <CodeMirror
+                value={jsonData?.content || ''}
+                height='400px'
+                extensions={[json()]}
+                editable={false}
+                className='border shadow-sm'
+            />
         </div>
     );
 }
