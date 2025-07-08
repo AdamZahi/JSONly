3799abd97c2de4eb5cb59e8fe4a228daa11c8605
Adam zahi <zahi.adem10@gmail.com>
2025-07-08 19:05:18 +0100

    fixing params error

--- app/api/json/[id]/route.ts
+++ app/api/json/[id]/route.ts
@@ -1,19 +1,24 @@
 import prisma from "@/lib/db";
-import { NextResponse, type NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-    request: NextRequest,
-    { params }: { params: { id: string } }
-    ) {
-    const { id } = params;
+export async function GET(request: NextRequest, 
+  { params }: { params: Promise<{ id: string }> },
+  ) {
+  const id  = (await params).id;
 
-    const jsonData = await prisma.jsonData.findUnique({
-        where: { id },
+  try {
+    const json = await prisma.jsonData.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        content: true,
+        createdAt: true
+      }
     });
-
-    if (!jsonData) {
-        return NextResponse.json({ error: "Not found" }, { status: 404 });
-    }
-
-    return NextResponse.json(jsonData);
+    return NextResponse.json(json);
+  } catch(error) {
+    console.error('Error fetching JSON data:', error);
+    return NextResponse.json({ error: 'Error fetching JSON data'}, { status: 500 })
+  }
 }
