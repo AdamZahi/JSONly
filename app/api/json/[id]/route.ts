c8843ad6cf08f7295de13087918e22030da777aa
Adam zahi <zahi.adem10@gmail.com>
2025-07-08 23:37:14 +0100

    deploy 2

--- app/api/json/[id]/route.ts
+++ app/api/json/[id]/route.ts
@@ -1,26 +1,30 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params }: {
-  params: {
-    id: string
-  }
-}) {
-  const { id } = params;
+export async function GET(
+    request: NextRequest,
+    context: { params: { id: string } }
+    ) {
+    const { id } = context.params;
 
-  try {
-    const json = await prisma.jsonData.findUnique({
-      where: { id },
-      select: {
-        id: true,
-        name: true,
-        content: true,
-        createdAt: true
-      }
-    });
-    return NextResponse.json(json);
-  } catch(error) {
-    console.error('Error fetching JSON data:', error);
-    return NextResponse.json({ error: 'Error fetching JSON data'}, { status: 500 })
-  }
+    try {
+        const json = await prisma.jsonData.findUnique({
+        where: { id },
+        select: {
+            id: true,
+            name: true,
+            content: true,
+            createdAt: true
+        }
+        });
+
+        if (!json) {
+        return NextResponse.json({ error: "Not found" }, { status: 404 });
+        }
+
+        return NextResponse.json(json);
+    } catch (error) {
+        console.error('Error fetching JSON data:', error);
+        return NextResponse.json({ error: 'Error fetching JSON data' }, { status: 500 });
+    }
 }
