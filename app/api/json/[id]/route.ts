3fe2e051c84391e9282deb4a92e15f5e79091670
Adam zahi <zahi.adem10@gmail.com>
2025-07-08 19:18:57 +0100

    common

--- app/api/json/[id]/route.ts
+++ app/api/json/[id]/route.ts
@@ -2,9 +2,9 @@ import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, 
-  { params }: { params: Promise<{ id: string }> },
+  { params }: { params: Promise<{ id: string }> }
   ) {
-  const id  = (await params).id;
+  const { id } = await params;
 
   try {
     const json = await prisma.jsonData.findUnique({
