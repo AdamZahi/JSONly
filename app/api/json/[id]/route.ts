c748361842f675746f0513388495e2f59e9450c4
Adam zahi <zahi.adem10@gmail.com>
2025-07-08 23:27:25 +0100

    fix route id

--- app/api/json/[id]/route.ts
+++ app/api/json/[id]/route.ts
@@ -1,36 +1,26 @@
-import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
 
-type RouteContext = {
-    params: {
-        id: string;
-    };
-};
+export async function GET(request: NextRequest, { params }: {
+  params: {
+    id: string
+  }
+}) {
+  const { id } = params;
 
-export async function GET(
-    request: NextRequest,
-    context: RouteContext
-    ) {
-    const { id } = context.params;
-
-    try {
-        const json = await prisma.jsonData.findUnique({
-        where: { id },
-        select: {
-            id: true,
-            name: true,
-            content: true,
-            createdAt: true
-        }
-        });
-
-        if (!json) {
-        return NextResponse.json({ error: "Not found" }, { status: 404 });
-        }
-
-        return NextResponse.json(json);
-    } catch (error) {
-        console.error('Error fetching JSON data:', error);
-        return NextResponse.json({ error: 'Error fetching JSON data' }, { status: 500 });
-    }
+  try {
+    const json = await prisma.jsonData.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        content: true,
+        createdAt: true
+      }
+    });
+    return NextResponse.json(json);
+  } catch(error) {
+    console.error('Error fetching JSON data:', error);
+    return NextResponse.json({ error: 'Error fetching JSON data'}, { status: 500 })
+  }
 }
